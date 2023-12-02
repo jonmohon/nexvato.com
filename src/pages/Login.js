7b1d325a2ba53fa36e5dfca9d45cabbf3a1e2c16
Jonathan Mohon <jonathan@webjoint.com>
2023-12-01 19:15:20 -0600

    login ui

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 
 const Login = () => {
   useEffect(() => {
-    window.location.href = 'https://nexvatoauth-dev.auth.us-west-2.amazoncognito.com/';
+    window.location.href = 'https://nexvatoauth-dev.auth.us-west-2.amazoncognito.com/login?response_type=code&client_id=3b3em3996f5vl9j2k63bv81h87&redirect_uri=https://app.nexvato.com/dashboard/ ';
   }, []);
 
   return (
