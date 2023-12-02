ffe8e675ea5871eb1e9e25ffd009243f4d0c7202
Jonathan Mohon <jonathan@webjoint.com>
2023-12-01 22:06:20 -0600

    ?

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-// App.js
 import React from 'react';
-import { Amplify, withAuthenticator } from 'aws-amplify';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-import '@aws-amplify/ui-react/styles.css';
-import awsExports from './aws-exports';
-import Dashboard from './pages/Dashboard';
-import './App.css';
+import { Amplify } from 'aws-amplify';
 
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-const App = () => (
-  <div className="app-container">
-    <Router>
-      <Route exact path="/">
-        <Redirect to="/dashboard" />
-      </Route>
-      <Route path="/dashboard">
-        {({ history }) => (
-          <Authenticator
-            onStateChange={(authState) => {
-              if (authState === 'signIn') {
-                history.push('/dashboard');
-              }
-            }}
-          >
-            {({ signOut, user }) => (
-              <Dashboard user={user} signOut={signOut} />
-            )}
-          </Authenticator>
-        )}
-      </Route>
-    </Router>
-  </div>
-);
-
-export default withAuthenticator(App);
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
