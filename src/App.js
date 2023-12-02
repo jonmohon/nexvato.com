ef3fbf275d3b035e878670b6ff68b77da029e5be
Jonathan Mohon <jonathan@webjoint.com>
2023-12-01 22:15:29 -0600

    css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
+import './App.css'; // Import the CSS file
+
 Amplify.configure(awsExports);
 
-export default function App() {
+const App = () => {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className="app-container"> {/* Use className instead of style */}
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user ? user.username : 'Guest'}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   );
-}
+};
+
+export default App;
