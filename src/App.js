32b7463de95f2119ebb60318ca61530fe38a557b
Jonathan Mohon <jonathan@webjoint.com>
2023-12-01 20:16:21 -0600

    middle page auth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from './aws-exports';
+
+import './App.css'; // Import your CSS file
+
 Amplify.configure(awsExports);
 
 export default function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className="app-container">
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user ? user.username : 'Guest'}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
+    </div>
   );
 }
